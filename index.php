6eeb56231f7b7bed4e663d394c6782a3a49e60ba
Kyle Angelich <kyleangelich1@gmail.com>
2024-02-19 16:06:14 -0800

    Remove bullet-points from <li>

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $tasks = getTasks($conn);
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>To Do List</title>
-        <!-- <link rel="stylesheet" href="styles.css"> -->
+        <link rel="stylesheet" href="styles.css">
         <script src="disableButton.js" defer></script>
     </head>
 
