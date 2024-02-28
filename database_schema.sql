653f13486257c55dd746da5d1bc2bf7f715f2683
Kyle Angelich <kyleangelich1@gmail.com>
2024-02-27 19:14:58 -0800

    Add comment to sql

--- database_schema.sql
+++ database_schema.sql
@@ -1,6 +1,8 @@
+-- Currently being used in myPHPadmin, not here.
+
 -- Create todos table
-CREATE TABLE todos (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    task VARCHAR(255),
-    created_at DATETIME
-);
+-- CREATE TABLE todos (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     task VARCHAR(255),
+--     created_at DATETIME
+-- );
